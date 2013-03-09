ac9169e56c965c27cee64734fe5ab24117628e35
ssherar <s.sherar@live.com>
2013-03-09 14:43:23 +0000

    Implementing the Event class

--- event-creation/main.cpp
+++ event-creation/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include "Event.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	cout << "Hello world";
+	cout << "Hello world" << endl;
+	Event e ("Hello", 1,2,3,4,5);
 }
-
-
