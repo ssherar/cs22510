ba8619a9b0fd27c105083cb2f0eda7fe124200c8
ssherar <s.sherar@live.com>
2013-03-12 11:16:08 +0000

    Basic log appending now working

--- event-manager/log.c
+++ event-manager/log.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 int log_file = -1;
-FILE* log_file_ptr = 0x0; 
+FILE* log_file_ptr = 0x0;
 
 struct flock* file_lock(short type, short whence) {
 	static struct flock ret;
@@ -22,10 +22,16 @@ void load_log_file(char filename[]) {
 	if(fcntl(log_file, F_SETLK, fl) != -1) {
 		printf("Is now locked\n");
 	}
+	log_file_ptr = fopen(filename, "a");
 }
 
 void close_log_file() {
+	fclose(log_file_ptr);
 	if(fcntl(log_file, F_SETLKW, file_lock(F_UNLCK, SEEK_SET)) != -1) {
 		printf("Is now unlocked\n");
 	}
+}
+
+void append_log_file() {
+	fprintf(log_file_ptr, "%s\n", "Hello World");
 }
--- event-manager/log.h
+++ event-manager/log.h
@@ -1,2 +1,3 @@
 int load_log_file(char filename[]);
 int close_log_file();
+void append_log_file();
