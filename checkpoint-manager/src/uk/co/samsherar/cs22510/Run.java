6c9c284b5c0fa07fed9c0ac60ddf2b138183677e
ssherar <s.sherar@live.com>
2013-03-13 19:43:42 +0000

    Adding commandline parsing for 4 files

--- checkpoint-manager/src/uk/co/samsherar/cs22510/Run.java
+++ checkpoint-manager/src/uk/co/samsherar/cs22510/Run.java
@@ -4,6 +4,15 @@ import uk.co.samsherar.cs22510.Controller.Manager;
 
 public class Run {
 	public static void main(String[] args) {
+		if(args.length != 4) {
+			printUsage();
+			System.exit(1);
+		}
+		
 		Manager.getInstance().runGUI();
+	}
+
+	private static void printUsage() {
+		System.out.println("Usage: CheckpointManager [Event Info File] [Course File] [Nodes File] [Entrant File] ");
 	}
 }
