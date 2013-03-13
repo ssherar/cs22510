05548856e9f6655ded9c0568ffa8689f18c4b498
ssherar <s.sherar@live.com>
2013-03-13 20:15:27 +0000

    Adding EventInfo model class

---
+++ checkpoint-manager/src/uk/co/samsherar/cs22510/Model/EventInfo.java
@@ -0,0 +1,58 @@
+package uk.co.samsherar.cs22510.Model;
+import java.util.*;
+
+
+public class EventInfo {
+	private String name;
+	private Date date;
+	private String time;
+
+	/**
+	 *
+	 */
+	public EventInfo() {
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	/**
+	 * @return the time
+	 */
+	public String getTime() {
+		return time;
+	}
+
+	/**
+	 * @param time the time to set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
