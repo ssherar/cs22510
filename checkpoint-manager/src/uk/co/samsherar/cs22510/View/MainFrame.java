0dd48e004cb24f44d820ebfc923add4eeae247a6
ssherar <s.sherar@live.com>
2013-03-21 00:35:52 +0000

    Adding basic gui

--- checkpoint-manager/src/uk/co/samsherar/cs22510/View/MainFrame.java
+++ checkpoint-manager/src/uk/co/samsherar/cs22510/View/MainFrame.java
@@ -2,19 +2,68 @@ package uk.co.samsherar.cs22510.View;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+	private String[] item1 = {
+			"One",
+			"Two",
+			"3"
+	};
+	
+	private String[] item2 = {
+			"1",
+			"2",
+			"3"
+	};
+	final JCheckBox medical;
+	final JTextField depart;
 	public MainFrame() {
-		this.setLayout(new BorderLayout());
+		this.setLayout(new GridLayout(0,2));
 		setTitle("Hello World");
-		setSize(800,600);
+		setSize(300,400);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		MainPane p = new MainPane();
-		LeftPane lp = new LeftPane();
-		this.add(lp, BorderLayout.WEST);	
-		this.add(p, BorderLayout.CENTER);
+		this.add(new JLabel("Entrants: "));
+		JComboBox entrants = new JComboBox(item1);
+		this.add(entrants);
+		
+		this.add(new JLabel("Checkpoints: "));
+		JComboBox checkpoints = new JComboBox(item2);
+		this.add(checkpoints);
+		
+		this.add(new JLabel("Medical Checkpoint? "));
+		medical = new JCheckBox();
+		medical.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				depart.setEnabled(medical.isSelected());
+			}
+			
+		});
+		this.add(medical);
+		
+		this.add(new JLabel("Arrival: "));
+		JTextField arrival = new JTextField();
+		this.add(arrival);
+		
+		this.add(new JLabel("Depart: "));
+		depart = new JTextField();
+		depart.setEnabled(false);
+		this.add(depart);
+		
+		JButton add = new JButton("Add");
+		JButton exit = new JButton("Exit");
+		
+		this.add(add);
+		this.add(exit);
+		
+		this.pack();
+		
 		this.repaint();
 		this.validate();
 		this.setVisible(true);
