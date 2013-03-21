a74448a3fa30b997864af1edcd6ae092503b0b5c
ssherar <s.sherar@live.com>
2013-03-21 23:58:40 +0000

    Cleaning and commenting

--- event-creation/Event.cpp
+++ event-creation/Event.cpp
@@ -5,6 +5,15 @@
 
 using namespace std;
 
+/**
+ * Creates an event
+ * @param info  information of the event
+ * @param day the day
+ * @param month the month
+ * @param year the year
+ * @param hour the hour
+ * @param minute the minute
+ */
 Event::Event(string info, int day, int month, int year, int hour, int minute) {
 	this->event_name = info;
 	this->day = day;
@@ -13,7 +22,11 @@ Event::Event(string info, int day, int month, int year, int hour, int minute) {
 	this->hour = hour;
 	this->minute = minute;
 }
-
+/**
+ * Creates a formatted string to be outputted to
+ * the screen
+ * @returns the formatted string
+ */
 string Event::format_for_screen() {
 	string ret;
 	if(this->event_name.empty() ||
@@ -41,10 +54,18 @@ string Event::format_for_screen() {
 				
 }
 
+/**
+ * Returns the name of the event
+ * @return the name of the event
+ */
 string Event::get_name() {
 	return this->event_name;
 }
 
+/**
+ * Returns the formatted date
+ * @return formatted string with date/time
+ */
 string Event::get_date() {
 	char formatted_time[100];
 	sprintf(formatted_time, "on %02d/%02d/%04d at %02d:%02d",
@@ -55,7 +76,3 @@ string Event::get_date() {
 				this->minute);
 	return formatted_time;
 }
-
-//char* Event::format_printing_file() {
-	
-//}
