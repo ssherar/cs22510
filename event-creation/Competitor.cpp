71f19ce8a04c13ed066c3b44c2ba4dfa0086707e
ssherar <s.sherar@live.com>
2013-03-21 23:32:35 +0000

    Adding params to comments

--- event-creation/Competitor.cpp
+++ event-creation/Competitor.cpp
@@ -7,6 +7,8 @@ using namespace std;
 /**
  * Constructor for the competitor, which sets the name
  * and course identifier
+ * @param course_id the course identifier
+ * @param name the name of the competitor
  */
 Competitor::Competitor(char course_id, string name) {
 	this->course_id = course_id;
